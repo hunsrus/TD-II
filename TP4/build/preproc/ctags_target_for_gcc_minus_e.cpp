958dbc01d98c551236692f9a3de0b2b4537a0e60
hunsrus <gabie00018@gmail.com>
2022-05-04 17:36:13 -0300

    switch between modes

--- TP4/build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ TP4/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,14 +1,19 @@
 # 1 "/media/gabriel/DATOS/Facultad/Ingeniería Electrónica/4/Técnicas Digitales II/Trabajos prácticos/TP4/lucesysonidos/lucesysonidos.ino"
 # 2 "/media/gabriel/DATOS/Facultad/Ingeniería Electrónica/4/Técnicas Digitales II/Trabajos prácticos/TP4/lucesysonidos/lucesysonidos.ino" 2
-# 56 "/media/gabriel/DATOS/Facultad/Ingeniería Electrónica/4/Técnicas Digitales II/Trabajos prácticos/TP4/lucesysonidos/lucesysonidos.ino"
-int pinBuz = 11;
-int pinBtn = 8;
+# 3 "/media/gabriel/DATOS/Facultad/Ingeniería Electrónica/4/Técnicas Digitales II/Trabajos prácticos/TP4/lucesysonidos/lucesysonidos.ino" 2
+
+
+
+
+
+
+
 int pinLED[] = {0,1,2,3,4,5,6,7};
-int *melodia, cantNotas, nota = 0;
-int cantEstados[7] = {17,14,8,8,8,0,0};
+int cantNotas, nota = 0;
+int cantEstados[7] = {17,14,8,8,8,11,11};
 int btnCount, state, pulsado = 0;
-unsigned long t1 = 0, elapsed = 0;
-bool mode = 0, finMelodia = 0, reset = 0;
+float t1 = 0, elapsed = 0;
+bool finMelodia = 0, reset = 0;
 
 int divider = 0, noteDuration = 0;
 //duración de una nota completa
@@ -81,10 +86,30 @@ int secuencia[7][17][8] = {
     {1,0,0,0,0,0,0,1}
   },
   {
-    {0}
+    {0,0,0,0,0,0,0,0},
+    {1,0,0,0,0,0,0,1},
+    {0,1,0,0,0,0,1,0},
+    {0,0,1,0,0,1,0,0},
+    {0,0,0,1,1,0,0,0},
+    {1,0,0,1,1,0,0,1},
+    {0,1,0,1,1,0,1,0},
+    {0,0,1,1,1,1,0,0},
+    {1,0,1,1,1,1,0,1},
+    {0,1,1,1,1,1,1,0},
+    {1,1,1,1,1,1,1,1}
   },
   {
-    {0}
+    {1,1,1,1,1,1,1,1},
+    {0,1,1,1,1,1,1,0},
+    {1,0,1,1,1,1,0,1},
+    {0,0,1,1,1,1,0,0},
+    {0,1,0,1,1,0,1,0},
+    {1,0,0,1,1,0,0,1},
+    {0,0,0,1,1,0,0,0},
+    {0,0,1,0,0,1,0,0},
+    {0,1,0,0,0,0,1,0},
+    {1,0,0,0,0,0,0,1},
+    {0,0,0,0,0,0,0,0}
   },
 };
 
@@ -122,46 +147,38 @@ int takeonme[] = {
 
 };
 
-int lcdtmAllBoys[] = {
-  294,8, 330,8, 330,8, 330,8, 330,8, 330,4, 294,8, 349,4, 0,6,
-  330,8, 294,8, 294,8, 294,8, 294,8, 294,4, 262,8, 330,4, 0,6,
-  262,8, 262,8, 262,8, 262,8, 262,8, 262,4, 494,6, 294,4, 294,8,
-  294,8, 262,8, 494,4, 220,8, 208,4, 208,8,
-  208,8, 208,8, 220,4, 494,8, 220,4, 0,3,
-};
-
 void setup() {
   pinMode(13, 0x1);
-  pinMode(pinBuz, 0x1);
-  pinMode(pinBtn, 0x1);
+  pinMode(11, 0x1);
+  pinMode(8, 0x1);
   for(int i = 0; i < 7; i++)
     pinMode(pinLED[i], 0x1);
-  btnCount = 11;
+  btnCount = 10;
   state = 0;
 }
 
 void loop() {
-  if(mode){
-    if(!digitalRead(pinBtn)) t1 = millis();
+  if(digitalRead(10)){
+    if(!digitalRead(8)) t1 = millis();
     else elapsed = millis() - t1;
     if(elapsed > 100)
     {
-      if(elapsed > 1000) btnCount = 11;
+      if(elapsed > 1000) btnCount = 10;
       else{
-        if(btnCount >= 10) btnCount = 0;
+        if(btnCount >= 10 -1) btnCount = 0;
         else btnCount++;
       }
       elapsed = 0;
     }
-    if(btnCount < 8)
+    if(btnCount < 7)
     {
       for(int i = 0; i < 8; i++)
         digitalWrite(pinLED[i],secuencia[btnCount][state][i]);
       if(state >= cantEstados[btnCount]-1) state = 0;
       else state++;
-    }else if(btnCount < 11)
+    }else if(btnCount < 10)
     {
-      int secMemPos = (btnCount-8)*(20+1);
+      int secMemPos = (btnCount-7)*(17 +1);
       int cantEstadosMem = EEPROM.read(secMemPos);
       for(int i = 0; i < 8; i++)
         digitalWrite(pinLED[i], (((EEPROM.read(secMemPos+state+1)) >> (i)) & 0x01));
@@ -175,12 +192,12 @@ void loop() {
     delay(200);
   }else
   {
-    if(antiRebote(pinBtn))
+    if(antiRebote(8))
     {
       t1 = millis();
       while((millis() - t1) < 1000)
       {
-        if(antiRebote(pinBtn))
+        if(antiRebote(8))
           reset = true;
       }
       if(!reset)
@@ -205,8 +222,8 @@ void loop() {
       finMelodia = reproducir(takeonme,nota);
       break;
     case 2:
-      cantNotas = sizeof(lcdtmAllBoys) / sizeof(int) / 2;
-      finMelodia = reproducir(lcdtmAllBoys,nota);
+      cantNotas = sizeof(felizcumple) / sizeof(int) / 2;
+      finMelodia = reproducir(felizcumple,nota);
       break;
     default:
       digitalWrite(13, !digitalRead(13));
@@ -246,11 +263,11 @@ bool reproducir(int melodia[], int nota)
       noteDuration *= 1.5; // incrementa la duración por la mitad
     }
     // suena la nota el 90% del tiempo estipulado y el resto es una pequeña pausa entre notas
-    tone(pinBuz, melodia[nota], noteDuration * 0.9);
+    tone(11, melodia[nota], noteDuration * 0.9);
     // espera el tiempo que dura la nota
     delay(noteDuration);
     // detiene la nota antes de la siguiente
-    noTone(pinBuz);
+    noTone(11);
     return false;
   }else return true;
 }
--- TP4/lucesysonidos/lucesysonidos.ino
+++ TP4/lucesysonidos/lucesysonidos.ino
@@ -1,66 +1,18 @@
 #include <EEPROM.h>
+#include "notas.h"
 
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_D2ST      0
-#define REST      0
 #define MAX_ESTADOS 17
 #define CANT_SEC    10
 #define TEMPO 140
-int pinBuz = 11;
-int pinBtn = 8;
+#define PIN_SLIDER 10
+#define PIN_BUZZER 11
+#define PIN_BUTTON 8
 int pinLED[] = {0,1,2,3,4,5,6,7};
-int *melodia, cantNotas, nota = 0;
-int cantEstados[7] = {17,14,8,8,8,0,0};
+int cantNotas, nota = 0;
+int cantEstados[7] = {17,14,8,8,8,11,11};
 int btnCount, state, pulsado = 0;
-unsigned long t1 = 0, elapsed = 0;
-bool mode = 0, finMelodia = 0, reset = 0;
+float t1 = 0, elapsed = 0;
+bool finMelodia = 0, reset = 0;
 
 int divider = 0, noteDuration = 0;
 //duración de una nota completa
@@ -133,87 +85,99 @@ int secuencia[7][MAX_ESTADOS][8] = {
     {1,0,0,0,0,0,0,1}
   },
   {
-    {0}
+    {0,0,0,0,0,0,0,0},
+    {1,0,0,0,0,0,0,1},
+    {0,1,0,0,0,0,1,0},
+    {0,0,1,0,0,1,0,0},
+    {0,0,0,1,1,0,0,0},
+    {1,0,0,1,1,0,0,1},
+    {0,1,0,1,1,0,1,0},
+    {0,0,1,1,1,1,0,0},
+    {1,0,1,1,1,1,0,1},
+    {0,1,1,1,1,1,1,0},
+    {1,1,1,1,1,1,1,1}
   },
   {
-    {0}
+    {1,1,1,1,1,1,1,1},
+    {0,1,1,1,1,1,1,0},
+    {1,0,1,1,1,1,0,1},
+    {0,0,1,1,1,1,0,0},
+    {0,1,0,1,1,0,1,0},
+    {1,0,0,1,1,0,0,1},
+    {0,0,0,1,1,0,0,0},
+    {0,0,1,0,0,1,0,0},
+    {0,1,0,0,0,0,1,0},
+    {1,0,0,0,0,0,0,1},
+    {0,0,0,0,0,0,0,0}
   },
 };
 
 int felizcumple[] = {
 
-  NOTE_C4,4, NOTE_C4,8, 
-  NOTE_D4,-4, NOTE_C4,-4, NOTE_F4,-4,
-  NOTE_E4,-2, NOTE_C4,4, NOTE_C4,8, 
-  NOTE_D4,-4, NOTE_C4,-4, NOTE_G4,-4,
-  NOTE_F4,-2, NOTE_C4,4, NOTE_C4,8,
+  NOTA_C4,4, NOTA_C4,8, 
+  NOTA_D4,-4, NOTA_C4,-4, NOTA_F4,-4,
+  NOTA_E4,-2, NOTA_C4,4, NOTA_C4,8, 
+  NOTA_D4,-4, NOTA_C4,-4, NOTA_G4,-4,
+  NOTA_F4,-2, NOTA_C4,4, NOTA_C4,8,
 
-  NOTE_C5,-4, NOTE_A4,-4, NOTE_F4,-4, 
-  NOTE_E4,-4, NOTE_D4,-4, NOTE_AS4,4, NOTE_AS4,8,
-  NOTE_A4,-4, NOTE_F4,-4, NOTE_G4,-4,
-  NOTE_F4,-2,
+  NOTA_C5,-4, NOTA_A4,-4, NOTA_F4,-4, 
+  NOTA_E4,-4, NOTA_D4,-4, NOTA_AS4,4, NOTA_AS4,8,
+  NOTA_A4,-4, NOTA_F4,-4, NOTA_G4,-4,
+  NOTA_F4,-2,
  
 };
 
 int takeonme[] = {
 
-  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
-  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
-  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
-  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
-  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
+  NOTA_FS5,8, NOTA_FS5,8,NOTA_D5,8, NOTA_B4,8, REST,8, NOTA_B4,8, REST,8, NOTA_E5,8, 
+  REST,8, NOTA_E5,8, REST,8, NOTA_E5,8, NOTA_GS5,8, NOTA_GS5,8, NOTA_A5,8, NOTA_B5,8,
+  NOTA_A5,8, NOTA_A5,8, NOTA_A5,8, NOTA_E5,8, REST,8, NOTA_D5,8, REST,8, NOTA_FS5,8, 
+  REST,8, NOTA_FS5,8, REST,8, NOTA_FS5,8, NOTA_E5,8, NOTA_E5,8, NOTA_FS5,8, NOTA_E5,8,
+  NOTA_FS5,8, NOTA_FS5,8,NOTA_D5,8, NOTA_B4,8, REST,8, NOTA_B4,8, REST,8, NOTA_E5,8, 
   
-  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
-  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
-  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
-  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
-  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
+  REST,8, NOTA_E5,8, REST,8, NOTA_E5,8, NOTA_GS5,8, NOTA_GS5,8, NOTA_A5,8, NOTA_B5,8,
+  NOTA_A5,8, NOTA_A5,8, NOTA_A5,8, NOTA_E5,8, REST,8, NOTA_D5,8, REST,8, NOTA_FS5,8, 
+  REST,8, NOTA_FS5,8, REST,8, NOTA_FS5,8, NOTA_E5,8, NOTA_E5,8, NOTA_FS5,8, NOTA_E5,8,
+  NOTA_FS5,8, NOTA_FS5,8,NOTA_D5,8, NOTA_B4,8, REST,8, NOTA_B4,8, REST,8, NOTA_E5,8, 
+  REST,8, NOTA_E5,8, REST,8, NOTA_E5,8, NOTA_GS5,8, NOTA_GS5,8, NOTA_A5,8, NOTA_B5,8,
   
-  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
-  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
+  NOTA_A5,8, NOTA_A5,8, NOTA_A5,8, NOTA_E5,8, REST,8, NOTA_D5,8, REST,8, NOTA_FS5,8, 
+  REST,8, NOTA_FS5,8, REST,8, NOTA_FS5,8, NOTA_E5,8, NOTA_E5,8, NOTA_FS5,8, NOTA_E5,8,
   
-};
-
-int lcdtmAllBoys[] = {
-  NOTE_D4,8, NOTE_E4,8, NOTE_E4,8, NOTE_E4,8, NOTE_E4,8, NOTE_E4,4, NOTE_D4,8, NOTE_F4,4, REST,6,
-  NOTE_E4,8, NOTE_D4,8, NOTE_D4,8, NOTE_D4,8, NOTE_D4,8, NOTE_D4,4, NOTE_C4,8, NOTE_E4,4, REST,6,
-  NOTE_C4,8, NOTE_C4,8, NOTE_C4,8, NOTE_C4,8, NOTE_C4,8, NOTE_C4,4, NOTE_B4,6, NOTE_D4,4, NOTE_D4,8,
-  NOTE_D4,8, NOTE_C4,8, NOTE_B4,4, NOTE_A3,8, NOTE_GS3,4, NOTE_GS3,8,
-  NOTE_GS3,8, NOTE_GS3,8, NOTE_A3,4, NOTE_B4,8, NOTE_A3,4, REST,3,
 };
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(pinBuz, OUTPUT);
-  pinMode(pinBtn, OUTPUT);
+  pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(PIN_BUTTON, OUTPUT);
   for(int i = 0; i < 7; i++)
     pinMode(pinLED[i], OUTPUT);
-  btnCount = 11;
+  btnCount = 10;
   state = 0;
 }
 
 void loop() {
-  if(mode){
-    if(!digitalRead(pinBtn)) t1 = millis();
+  if(digitalRead(PIN_SLIDER)){
+    if(!digitalRead(PIN_BUTTON)) t1 = millis();
     else elapsed = millis() - t1;
     if(elapsed > 100)
     {
-      if(elapsed > 1000) btnCount = 11;
+      if(elapsed > 1000) btnCount = 10;
       else{
-        if(btnCount >= CANT_SEC) btnCount = 0;
+        if(btnCount >= CANT_SEC-1) btnCount = 0;
         else btnCount++;
       }
       elapsed = 0;
     }
-    if(btnCount < 8)
+    if(btnCount < 7)
     {
       for(int i = 0; i < 8; i++)
         digitalWrite(pinLED[i],secuencia[btnCount][state][i]);
       if(state >= cantEstados[btnCount]-1) state = 0;
       else state++;
-    }else if(btnCount < 11)
+    }else if(btnCount < 10)
     {
-      int secMemPos = (btnCount-8)*(20+1);
+      int secMemPos = (btnCount-7)*(MAX_ESTADOS+1);
       int cantEstadosMem = EEPROM.read(secMemPos);
       for(int i = 0; i < 8; i++)
         digitalWrite(pinLED[i], bitRead(EEPROM.read(secMemPos+state+1), i));
@@ -227,12 +191,12 @@ void loop() {
     delay(200);
   }else
   {
-    if(antiRebote(pinBtn))
+    if(antiRebote(PIN_BUTTON))
     {
       t1 = millis();
       while((millis() - t1) < 1000)
       {
-        if(antiRebote(pinBtn))
+        if(antiRebote(PIN_BUTTON))
           reset = true;
       }
       if(!reset)
@@ -257,8 +221,8 @@ void loop() {
       finMelodia = reproducir(takeonme,nota);
       break;
     case 2:
-      cantNotas = sizeof(lcdtmAllBoys) / sizeof(int) / 2;
-      finMelodia = reproducir(lcdtmAllBoys,nota);
+      cantNotas = sizeof(felizcumple) / sizeof(int) / 2;
+      finMelodia = reproducir(felizcumple,nota);
       break;
     default:
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
@@ -298,11 +262,11 @@ bool reproducir(int melodia[], int nota)
       noteDuration *= 1.5; // incrementa la duración por la mitad
     }
     // suena la nota el 90% del tiempo estipulado y el resto es una pequeña pausa entre notas
-    tone(pinBuz, melodia[nota], noteDuration * 0.9);
+    tone(PIN_BUZZER, melodia[nota], noteDuration * 0.9);
     // espera el tiempo que dura la nota
     delay(noteDuration);
     // detiene la nota antes de la siguiente
-    noTone(pinBuz);
+    noTone(PIN_BUZZER);
     return false;
   }else return true;
 }
