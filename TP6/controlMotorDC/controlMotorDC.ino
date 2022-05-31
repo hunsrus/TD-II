4157aa0fa38f5896b51df0193c912b2471f794e1
hunsrus <gabie00018@gmail.com>
2022-05-31 18:13:30 -0300

    add remote control

--- TP6/controlMotorDC/controlMotorDC.ino
+++ TP6/controlMotorDC/controlMotorDC.ino
@@ -3,13 +3,20 @@
 #define VEL_STEP 26
 #define M_STOPPED 0
 #define M_RUNNING 1
+#define M_MANUAL 0
+#define M_REMOTE 1
+#define COOLDOWN_MS 100
 
 int pinPWM = 3;
 int pinOnOff = 2;
 int pinVelUp = 7;
 int pinVelDown = 8;
+unsigned long coolDown;
 byte vel = 128;
 bool state = M_STOPPED;
+bool mode = M_MANUAL;
+bool stringComplete;
+String inputString;
 
 void setup()
 {
@@ -19,20 +26,106 @@ void setup()
     pinMode(pinVelUp, INPUT);
     pinMode(pinVelDown, INPUT);
     attachInterrupt(digitalPinToInterrupt(pinOnOff), switchMotorOnOff, RISING);
+    coolDown = millis();
+
+    Serial.begin(9600);
+    inputString.reserve(200);
 }
 
 void loop()
 {
     if(state == M_RUNNING)
     {
-        if(digitalRead(pinVelUp) && (vel < VEL_MAX)) vel += VEL_STEP;
-        else if(digitalRead(pinVelDown) && (vel > VEL_MIN)) vel -= VEL_STEP;
-        analogWrite(pinPWM,vel);
+        if(millis() - coolDown > COOLDOWN_MS)
+        {
+            if(digitalRead(pinVelUp)) motorVelUp();
+            else if(digitalRead(pinVelDown)) motorVelDown();
+        }
+    }else
+    {
+        vel = 0;
+        digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+        delay(200);
     }
+    analogWrite(pinPWM,vel);
 }
 
 void switchMotorOnOff()
 {
-    digitalWrite(pinPWM, 0);
-    state = !state;
+    if((millis() - coolDown > COOLDOWN_MS) && (mode == M_MANUAL))
+    {
+        state = !state;
+        vel = 128;
+        coolDown = millis();
+        digitalWrite(LED_BUILTIN,HIGH);
+    }
+}
+
+void motorVelUp()
+{
+    if(vel < VEL_MAX) vel += VEL_STEP;
+    coolDown = millis();
+}
+void motorVelDown()
+{
+    if(vel > VEL_MIN) vel -= VEL_STEP;
+    coolDown = millis();
+}
+bool setMotorVel(float velPercent)
+{
+    bool ret = true;
+    if((velPercent <= 100) && (velPercent >= 0)) vel = velPercent*255/100;
+    else ret = false;
+    return ret;
+}
+
+void serialEvent()
+{
+    while (Serial.available())
+    {
+        char inChar = (char)Serial.read();
+        char code;
+        String ret = "$TD2,OK*";
+        inputString += inChar;
+        if (inChar == '\n')
+        {
+            if(inputString.substring(0,4).equals("$TD2,"))
+            {
+                code = inputString.charAt(5);
+                switch (code)
+                {
+                    case 'a':
+                        mode = M_REMOTE;
+                    break;
+                    case 'b':
+                        mode = M_MANUAL;
+                    break;
+                    case 'c':
+                        state = M_RUNNING;
+                        vel = 128;
+                        digitalWrite(LED_BUILTIN,HIGH);
+                    break;
+                    case 'd':
+                        state = M_STOPPED;
+                        vel = 128;
+                    break;
+                    case 'e':
+                        motorVelUp();
+                    break;
+                    case 'f':
+                        motorVelDown();
+                    break;
+                    case 'g':
+                        float velPercent = inputString.substring(7,inputString.indexOf('*')-1).toFloat();
+                        if(!setMotorVel(velPercent)) ret = "$TD2,Err*";
+                    break;
+                    default:
+                        ret = "$TD2,Err*";
+                    break;
+                }
+            }
+            Serial.println(ret);
+            inputString = "";
+        }
+    }
 }
