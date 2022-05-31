7c66bb15dbf4b2c0810a42c32cbb89cf96f0fa14
hunsrus <gabie00018@gmail.com>
2022-05-31 16:47:11 -0300

    add cooldown and status LED

--- TP5/controlMotorDC/controlMotorDC.ino
+++ TP5/controlMotorDC/controlMotorDC.ino
@@ -3,11 +3,13 @@
 #define VEL_STEP 26
 #define M_STOPPED 0
 #define M_RUNNING 1
+#define COOLDOWN_MS 100
 
 int pinPWM = 3;
 int pinOnOff = 2;
 int pinVelUp = 7;
 int pinVelDown = 8;
+unsigned long coolDown;
 byte vel = 128;
 bool state = M_STOPPED;
 
@@ -19,20 +21,39 @@ void setup()
     pinMode(pinVelUp, INPUT);
     pinMode(pinVelDown, INPUT);
     attachInterrupt(digitalPinToInterrupt(pinOnOff), switchMotorOnOff, RISING);
+    coolDown = millis();
 }
 
 void loop()
 {
     if(state == M_RUNNING)
     {
-        if(digitalRead(pinVelUp) && (vel < VEL_MAX)) vel += VEL_STEP;
-        else if(digitalRead(pinVelDown) && (vel > VEL_MIN)) vel -= VEL_STEP;
-        analogWrite(pinPWM,vel);
+        if(millis() - coolDown > COOLDOWN_MS)
+            if(digitalRead(pinVelUp) && (vel < VEL_MAX))
+            {
+                vel += VEL_STEP;
+                coolDown = millis();
+            }else if(digitalRead(pinVelDown) && (vel > VEL_MIN))
+            {
+                vel -= VEL_STEP;
+                coolDown = millis();
+            }
+    }else
+    {
+        vel = 0;
+        digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+        delay(200);
     }
+    analogWrite(pinPWM,vel);
 }
 
 void switchMotorOnOff()
 {
-    digitalWrite(pinPWM, 0);
-    state = !state;
+    if(millis() - coolDown > COOLDOWN_MS)
+    {
+        state = !state;
+        vel = 128;
+        coolDown = millis();
+        digitalWrite(LED_BUILTIN,HIGH);
+    }
 }
